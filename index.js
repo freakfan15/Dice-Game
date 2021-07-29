f98a002b9dc7aac192dac3ee4cdb02a13194095f
freakfan15 <vivekkrrai.bxr@gmail.com>
2021-07-29 20:52:20 +0530

    finished

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1*6;
+randomNumber1 = Math.floor(randomNumber1) +1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var dice1 = document.querySelector("img.img1");
+dice1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var dice2 = document.querySelector("img.img2");
+dice2.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
